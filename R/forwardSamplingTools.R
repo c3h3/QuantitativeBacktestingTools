b39c9f8df244844504ed9b9b1195915839308f72
c3h3 <c3h3.tw@gmail.com>
2016-09-08 02:11:40 +0800

    update documnents

--- R/forwardSamplingTools.R
+++ R/forwardSamplingTools.R
@@ -5,8 +5,10 @@
 
 #' Pt2Rt
 #'
-#' Pt2Rt
+#' function which can help you transform your price xts to an accumulate return xts
 #'
+#' @param Xt a xts object
+#' @param XtColumnSeletor Xt's single column selector
 #' @examples
 #' Xt = getSymbols('2330.TW', auto.assign = F)
 #' Xt %>% Pt2Rt %>% head
@@ -25,8 +27,10 @@ Pt2Rt <- function(Xt, XtColumnSeletor = Cl) {
 
 #' ForwardSlidingWindow
 #'
-#' ForwardSlidingWindow
+#' a funtion which an help you do forward sampling easier
 #'
+#' @param accRt an accumulate return xts object
+#' @param win_size the size of forward sampling slide window
 #' @examples
 #' Xt = getSymbols('2330.TW', auto.assign = F)
 #' Xt %>% Pt2Rt %>% ForwardSlidingWindow(20) %>% head
@@ -46,8 +50,10 @@ ForwardSlidingWindow <- function(accRt, win_size) {
 
 #' FswXt2Df
 #'
-#' FswXt2Df
+#' a function which can help you transform your forward sampling xts to data.frame
 #'
+#' @param fswXt a forward sampling xts
+#' @param filterDatetimes filtered fswXt by datetimes
 #' @examples
 #' Xt = getSymbols('2330.TW', auto.assign = F)
 #' Xt %>% Pt2Rt %>% ForwardSlidingWindow(20) %>% FswXt2Df(index(Xt)[c(1,3,5,7,9)]) %>% head
